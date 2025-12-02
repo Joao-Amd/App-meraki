6b715bfae36a2f69dd57ada69a639a8e90ff989f
João Vitor de Almeida Medeiros <joao.medeiros@useall365.onmicrosoft.com>
2025-12-02 08:16:27 -0300

    Ajustado index.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+const postcss = require("./postcss.config.js");
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
-    open: true, 
+    open: true,
   },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  css: {
+    postcss, 
   },
 }));
